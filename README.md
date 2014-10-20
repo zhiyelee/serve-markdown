72aca37113a394857461ea9627c1146172cdae9b
zhiyelee <zhiyelee@gmail.com>
2014-10-20 19:26:42 +0800

    v0.1.0

--- README.md
+++ README.md
@@ -1,4 +1,34 @@
 serve-markdown
 ==============
 
+[![NPM Version][npm-image]][npm-url]
+[![NPM Downloads][downloads-image]][downloads-url]
+[![Build Status][travis-image]][travis-url]
+[![Test Coverage][coveralls-image]][coveralls-url]
+[![Gratipay][gratipay-image]][gratipay-url]
+
 serve markdown files
+
+## Install
+
+```sh
+$ npm install serve-markdown
+```
+
+## API
+
+```js
+var serveStatic = require(‘serve-markdown’)
+```
+
+### serveStatic(root, options)
+
+Create a new middleware function to serve markdown files from within a given root
+directory. The file to serve will be determined by combining `req.url`
+with the provided root directory. When a file is not found, instead of
+sending a 404 response, this module will instead call `next()` to move on
+to the next middleware, allowing for stacking and fall-backs.
+
+## Examples
+
+To be continued..
---
+++ index.js
@@ -0,0 +1,45 @@
+var marked = require('marked');
+var merge = require('utils-merge');
+var path = require('path');
+var fs = require('fs');
+
+exports = module.exports = function serveMarkdown(root, options) {
+    if (!root) {
+        throw new TypeError('root path required')
+    }
+
+    options = options || {};
+    initMarked(options.mdOptions);
+
+    return function (req, res, next) {
+        var fp = req.url;
+        var ext = path.extname(fp).substr(1);
+
+        fp = path.join(root, fp);
+        var isExists = fs.existsSync(fp);
+        if (isExists && (ext === 'md' || ext === 'markdown')) {
+            var html = marked(fs.readFileSync(fp, 'utf8'));
+            html = '<div class="markdown markdown-content">' + html + '</div>';
+            res.setHeader('Content-Type', 'text/html; charset=utf-8');
+            res.end(html);
+        } else {
+            next();
+        }
+    }
+};
+
+function initMarked(options) {
+    var mdOptions = {
+        renderer: new marked.Renderer(),
+        gfm: true,
+        tables: true,
+        breaks: false,
+        pedantic: false,
+        sanitize: true,
+        smartLists: true,
+        smartypants: false
+    };
+
+    mdOptions = merge(mdOptions, options)
+    marked.setOptions(mdOptions);
+}
