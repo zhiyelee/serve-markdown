053d7b6ae3f600ee00d4b983fe7cd678b9487a9e
zhiyelee <zhiyelee@gmail.com>
2014-11-23 23:33:59 +0800

    title test suite

--- test/index.js
+++ test/index.js
@@ -10,17 +10,46 @@ var root = path.resolve(__dirname, './fixtures/');
 describe('Server-markdown', function () {
     describe('title', function () {
         it('title is a string', function (done) {
-            app = connect();
             var option = {
                 template: '{{content}}',
                 title: 'test-server'
             };
-
-            app.use(serveMarkdown(root, option));
+            app = getApp(app, option);
 
             request(app)
                 .get('/p.md')
                 .expect(200, '<p>serve-markdown</p>\n', done)
         });
+        it('title is a function', function (done) {
+            var option = {
+                template: '{{title}}{{content}}',
+                title: function (name) {
+                    return name + '-fun';
+                }
+            };
+            app = getApp(app, option);
+
+            request(app)
+                .get('/p.md')
+                .expect(200, 'p.md-fun<p>serve-markdown</p>\n', done)
+        });
+        it('use default title', function (done) {
+            var option = {
+                template: '{{title}}{{content}}'
+            };
+            app = getApp(app, option);
+
+            request(app)
+                .get('/p.md')
+                .expect(200, 'p.md<p>serve-markdown</p>\n', done)
+        });
     });
 });
+
+
+function getApp(app, option) {
+    app = connect();
+    app.use(serveMarkdown(root, option));
+
+    return app;
+}
