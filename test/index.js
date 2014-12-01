c82c5bc225f5bf225d32a2f1e13ade07e3a32c7a
zhiyelee <zhiyelee@gmail.com>
2014-12-02 01:03:43 +0800

    add more tests

--- test/index.js
+++ test/index.js
@@ -2,6 +2,7 @@ var connect = require('connect');
 var request = require('supertest');
 var serveMarkdown = require('../');
 var path = require('path');
+var fs = require('fs');
 
 var app;
 var root = path.resolve(__dirname, './fixtures/');
@@ -41,7 +42,33 @@ describe('Server-markdown', function () {
 
             request(app)
                 .get('/p.md')
-                .expect(200, 'p.md<p>serve-markdown</p>\n', done)
+                .expect(200, 'p.md<p>serve-markdown</p>\n', done);
+        });
+    });
+    describe('use path in option', function () {
+        it('Both template and style are path', function (done) {
+            var option = {
+                template: path.resolve(__dirname, './fixtures/template.html'),
+                style: path.resolve(__dirname, './fixtures/screen.css')
+            };
+            app = getApp(app, option);
+
+            var expPath = path.resolve(__dirname,'./expected/p.html');
+            // trim the \n
+            // TODO why the \n
+            var expected = fs.readFileSync(expPath, 'utf8')
+                .replace(/\n/g, '')
+                .replace(/ +/g, ' ');
+
+            request(app)
+                .get('/p.md')
+                .expect(function (res) {
+                    res.res.text
+                        .replace(/\n/g, '')
+                        .replace(/ +/g, ' ')
+                        .should.equal(expected);
+                })
+                .end(done);
         });
     });
 });
