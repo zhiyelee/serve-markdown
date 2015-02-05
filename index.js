b7398023bca0780e643adc41587ca376866e808b
zhiyelee <zhiyelee@gmail.com>
2015-02-05 11:51:37 +0800

    format code

--- index.js
+++ index.js
@@ -6,82 +6,82 @@ var marked = require('marked')
   , fs = require('fs');
 
 exports = module.exports = function serveMarkdown(root, options) {
-    if (!root) {
-        throw new TypeError('root path required')
+  if (!root) {
+    throw new TypeError('root path required')
+  }
+
+  options = merge({
+    template: path.join(__dirname, 'public', 'template.html'),
+    title: function (fileName) {
+      return fileName;
+    },
+    classes: 'sm-content',
+    style: path.join(__dirname, 'public', 'screen.css')
+  }, options);
+
+  initMarked(options.mdOptions);
+
+  return function (req, res, next) {
+    var fp = req.url;
+    var ext = path.extname(fp).substr(1);
+
+    fp = path.join(root, fp);
+    fp = decode(fp);
+    // path contain invalid chars
+    if (!~fp) {
+      next();
     }
 
-    options = merge({
-        template: path.join(__dirname, 'public', 'template.html'),
-        title: function (fileName) {
-            return fileName;
-        },
-        classes: 'sm-content',
-        style: path.join(__dirname, 'public', 'screen.css')
-    }, options);
+    var isExists = fs.existsSync(fp);
+    if (isExists && (ext === 'md' || ext === 'markdown')) {
+      var html = marked(fs.readFileSync(fp, 'utf8'));
 
-    initMarked(options.mdOptions);
+      var template = options.template;
+      if (fs.existsSync(template) && fs.statSync(template).isFile()) {
+        template = fs.readFileSync(template, 'utf8');
+      }
 
-    return function (req, res, next) {
-        var fp = req.url;
-        var ext = path.extname(fp).substr(1);
+      var title = options.title;
+      if (typeof title === 'function') {
+        title = title(path.basename(fp));
+      }
 
-        fp = path.join(root, fp);
-        fp = decode(fp);
-        // path contain invalid chars
-        if (!~fp) {
-            next();
-        };
+      var style = options.style;
+      if (fs.existsSync(style) && fs.statSync(style).isFile()) {
+        style = fs.readFileSync(style, 'utf8');
+      }
 
-        var isExists = fs.existsSync(fp);
-        if (isExists && (ext === 'md' || ext === 'markdown')) {
-            var html = marked(fs.readFileSync(fp, 'utf8'));
+      var classes = options.classes;
+      if (Array.isArray(classes)) {
+        classes = classes.join(' ')
+      }
 
-            var template = options.template;
-            if (fs.existsSync(template) && fs.statSync(template).isFile()) {
-                template = fs.readFileSync(template, 'utf8');
-            }
+      html = template
+        .replace(/{{title}}/g, title)
+        .replace(/{{style}}/g, style)
+        .replace(/{{classes}}/g, classes)
+        .replace(/{{content}}/g, html);
 
-            var title = options.title;
-            if (typeof title === 'function') {
-                title = title(path.basename(fp));
-            }
-
-            var style = options.style;
-            if (fs.existsSync(style) && fs.statSync(style).isFile()) {
-                style = fs.readFileSync(style, 'utf8');
-            }
-
-            var classes = options.classes;
-            if (Array.isArray(classes)) {
-                classes = classes.join(' ')
-            }
-
-            html = template
-                    .replace(/{{title}}/g, title)
-                    .replace(/{{style}}/g, style)
-                    .replace(/{{classes}}/g, classes)
-                    .replace(/{{content}}/g, html);
-
-            res.setHeader('Content-Type', 'text/html; charset=utf-8');
-            res.end(html);
-        } else {
-            next();
-        }
+      res.setHeader('Content-Type', 'text/html; charset=utf-8');
+      res.end(html);
+    } else {
+      next();
     }
+  }
 };
 
 function initMarked(options) {
-    var mdOptions = {
-        renderer: new marked.Renderer(),
-        gfm: true,
-        tables: true,
-        breaks: true,
-        pedantic: false,
-        sanitize: true,
-        smartLists: true,
-        smartypants: false
-    };
+  var mdOptions = {
+    renderer: new marked.Renderer(),
+    gfm: true,
+    tables: true,
+    breaks: true,
+    pedantic: false,
+    sanitize: true,
+    smartLists: true,
+    smartypants: false
+  };
 
-    mdOptions = merge(mdOptions, options)
-    marked.setOptions(mdOptions);
+  mdOptions = merge(mdOptions, options)
+  marked.setOptions(mdOptions);
 }
